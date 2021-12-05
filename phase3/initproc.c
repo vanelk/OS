223d6a93bc8c72bf11aea09e4c09e7e6f93775f7
vanelk <ditrix.360@gmail.com>
2021-12-04 19:36:23 -0500

    compiled changes

--- phase3/initproc.c
+++ phase3/initproc.c
@@ -18,26 +18,26 @@ extern void pager();
 
 
 void test() {
-    initTLBSupport(); // this also
+    /*initTLBSupport();*/
     int pid = 1;
     state_PTR procState;
     for (pid; pid<MAXPROC+ONE; pid++) {
         procState->s_sp = 0xc0000000;
         procState->s_pc = procState->s_t9 = (memaddr)0x800000B0;
         procState->s_status = ALLOFF | KUON | TEBITON | IEON | IMON;
-        procState->s_entryHI = pid >> ASIDSHIFT;
+        procState->s_entryHI = pid << ASIDSHIFT;
         int i = 0;
         for(i; i < POOLSIZE; i++) {
-           supports[pid].sup_privatPgTb[i].entryHI = ((0x80000 + i) >> ASIDSHIFT) | (pid >> ASIDSHIFT);
+           supports[pid].sup_privatPgTb[i].entryHI = ((0x80000 + i) << VPNSHIFT) | (pid << ASIDSHIFT);
            supports[pid].sup_privatPgTb[i].entryLO = ALLOFF | DIRTYON;
         }
         supports[pid].sup_asid = pid;
-        supports[pid].sup_privatPgTb[POOLSIZE-1].entryHI = 0xBFFFF | 0x00000000 + pid;
+        supports[pid].sup_privatPgTb[POOLSIZE-1].entryHI = (0xBFFFF << VPNSHIFT) | (pid << ASIDSHIFT);
         supports[pid].sup_exceptContext[GENERALEXCEPT].c_stackPtr = (&supports[pid].sup_stackGen[500]);
-        supports[pid].sup_exceptContext[GENERALEXCEPT].c_status = ALLOFF | IEON | KUON | TEBITON;
-        supports[pid].sup_exceptContext[GENERALEXCEPT].c_pc =  (memaddr) uSyscallHandler; //
+        supports[pid].sup_exceptContext[GENERALEXCEPT].c_status = ALLOFF | IEON | TEBITON | IMON;
+        supports[pid].sup_exceptContext[GENERALEXCEPT].c_pc =  (memaddr) uSyscallHandler; 
         supports[pid].sup_exceptContext[PGFAULTEXCEPT].c_stackPtr = (&supports[pid].sup_stackTLB[500]);
-        supports[pid].sup_exceptContext[PGFAULTEXCEPT].c_status = ALLOFF | IEON | KUON | TEBITON;
+        supports[pid].sup_exceptContext[PGFAULTEXCEPT].c_status = ALLOFF | IEON | TEBITON | IMON;
         supports[pid].sup_exceptContext[PGFAULTEXCEPT].c_pc =  (memaddr) pager;
         SYSCALL(CREATEPROCESS, (int)&procState, (int) &supports[pid], 0);
     }
